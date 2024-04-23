59bd32d826f77bc92bc456866726342d142068ed
Wizqqr <baban1979molotov@gmail.com>
2024-04-23 18:24:55 +0600

    first commit

---
+++ src/Components/Picture/Gogh/Gogh.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Gogh = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Gogh
---
+++ src/Components/Picture/Picasso/Picasso.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Picasso = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Picasso
